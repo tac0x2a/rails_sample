5bcce0965f765935545bccaf35e0c3cd0c92e23b
TAC <tac@tac42.net>
2017-07-09 16:31:46 +0900

    finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #root "application#goodbye"
-  root "users#index"
+  # root "users#index"
+  root "static_pages#home"
 end
