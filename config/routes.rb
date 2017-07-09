086c60f8549ee795dfc068b7dfbc03d09ae5041c
TAC <tac@tac42.net>
2017-07-09 16:22:01 +0900

    dinamic pages test

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :microposts
+  get 'static_pages/help'
+
+  get 'static_pages/home'
+
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #root "application#goodbye"
